ee6cd858212c976ffa8dd0a5dcd18a5532520fc2
ryanesguera2002 <renzesguera@ckcm.edu.ph>
2024-02-18 08:56:32 +0800

    2nd commity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+function renderExpenses(expenses) {
+     const rows = expenses.map((expense) => {
+       return (
+         <tr key={expense._id}>
+           <td>{expense._id}</td>
+           <td>{expense.description}</td>
+           <td>{expense.amount}</td>
+          <td>{new Date(expense.date).toDateString()}</td>
+        </tr>
+       );
+    });
+  
+    return rows;
+   }
 function App() {
+  const [expenses, setExpenses] = useState([]);
+  const fetchExpenses = async () => {
+        const apiUrl = "https://workshop-expense-api-ryan.onrender.com";
+    
+        const endpoint = `${apiUrl}/api/expenses`;
+    
+        const response = await fetch(endpoint);
+    
+        const expenseData = await response.json();
+    
+        setExpenses(expenseData);
+      };
+        useEffect(() => {
+            fetchExpenses();
+          }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <textarea cols="30" rows="10"></textarea>
+        <input type="number" />
+        <input type="date" />
+        <button>Save</button>
+      </form>
+
+      <h2>My Expenses</h2>
+
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+       <tbody>
+       {renderExpenses(expenses)}
+        </tbody>
+      </table>
     </div>
   );
 }
